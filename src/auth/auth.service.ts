8627cc4e27f4b0e79650bd87a9cd8f5ab2add638
shbaek1997 <shbaek1997@gmail.com>
2022-10-30 22:40:33 +0900

    Feat: auth service validateUser

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
-
+import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService : UserService) {}
+
+  async validateUser(email: string, plainPassword: string): Promise<any> {
+    const user = await this.userService.findUserByEmail(email);
+    const { password } = user;
+    const passwordMatch = await bcrypt.compare(plainPassword, password);
+    if (user && passwordMatch) {
+      const { nickname, password, ...rest } = user;
+      return rest;
+    }
+    return null;
+  }
+}
