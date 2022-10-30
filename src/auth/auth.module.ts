fd66cde17588eb1ba27280f57a31494899ee6b54
shbaek1997 <shbaek1997@gmail.com>
2022-10-30 23:19:41 +0900

    Feat: added export for auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport/dist';
 import { UserModule } from 'src/user/user.module';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports: [UserModule, PassportModule],
-  controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -8,5 +8,6 @@ import { User } from 'src/typeorm/entities/User';
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
