01acefe307fbb552e6412b6f85504e4dbf5f4f07
shbaek1997 <shbaek1997@gmail.com>
2022-10-31 00:30:13 +0900

    Feat: add session-serializer

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { PassportModule } from '@nestjs/passport/dist';
 import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
+import { SessionSerializer } from './session.serializer';
 
 @Module({
-  imports: [UserModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [UserModule, PassportModule.register({ session: true })],
+  providers: [AuthService, LocalStrategy, SessionSerializer],
   exports: [AuthService],
 })
 export class AuthModule {}
