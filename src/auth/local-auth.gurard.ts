9efe4d5b92afe9c8b6b1038b6f6a5b9c67bb3f8f
shbaek1997 <shbaek1997@gmail.com>
2022-10-31 00:34:47 +0900

    Fix: added execution context for local auth guard

--- src/auth/local-auth.gurard.ts
+++ src/auth/local-auth.gurard.ts
@@ -1,6 +1,13 @@
 import { AuthGuard } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, ExecutionContext } from '@nestjs/common';
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const result = (await super.canActivate(context)) as boolean;
+    await super.logIn(request);
+    return result;
+  }
+}
 // similar to passport.authenticate('local')
 // so it has to be 'local' as specified in passport local strategy;
