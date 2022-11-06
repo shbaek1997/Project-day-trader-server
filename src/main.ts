d9edfd208b96295cbd29681cfe80525c679c7f5c
shbaek1997 <shbaek1997@gmail.com>
2022-11-07 00:48:03 +0900

    Fix: cors and cookie issue for log in

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ import * as SessionFileStore from 'session-file-store';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const FileStore = SessionFileStore(session);
-  app.enableCors();
+  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
   app.use(
     session({
       secret: process.env.SESSION_SECRET,
       name: 'test-cookie',
       resave: false,
       saveUninitialized: false,
-      cookie: { maxAge: 7200000 }, //2hrs
+      cookie: { maxAge: 7200000, secure: false }, //2hrs
       store: new FileStore(),
       //그냥 express-session은 memory에 session을 할당하고, 이것은 production에 사용하면 안됨.
       // 모든 session이 같이 날라갈(휘발) 가능성이 높아져서 다른 db 나 파일등에 저장해야함.
